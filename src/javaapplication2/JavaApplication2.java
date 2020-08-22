36cc89ef649060c7d460a4869213ee99d8179ebf
lldortechll <soylu.cihan.14@gmail.com>
2020-08-22 17:57:32 +0300

    ilk yükleme

---
+++ src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,54 @@
+
+package javaapplication2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class JavaApplication2 {
+
+   
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+    System.out.print(" x1, y1, x2, y2, x3, y3, x4, y4: ");
+    double x1 = input.nextDouble();
+    double y1 = input.nextDouble();
+    double x2 = input.nextDouble();
+    double y2 = input.nextDouble();
+    double x3 = input.nextDouble();
+    double y3 = input.nextDouble();
+    double x4 = input.nextDouble();
+    double y4 = input.nextDouble();
+
+    input.close();
+
+    double a = y1 - y2;
+    double b = -(x1 - x2);
+    double c = y3 - y4;
+    double d = -(x3 - x4);
+    double e = (y1 - y2) * x1 - (x1 - x2) * y1;
+    double f = (y3 - y4) * x3 - (x3 - x4) * y3;
+
+    double abBC = a * d - b * c;
+    double x = (e * d - b * f) / abBC;
+    double y = (a * f - e * c) / abBC;
+
+        
+        if(x1==y1&& x2==y2&&x3==y3&&x4==y4){
+            System.out.println("çakışıkkkkk");
+        }
+   
+    
+    if (abBC == 0) {
+        System.out.println("Doğrular parallel");
+       
+    } else {
+        System.out.println("kesişim noktası (" + x + ", " + y
+                + ")");
+    }
+    
+    
+    }
+            
+}
